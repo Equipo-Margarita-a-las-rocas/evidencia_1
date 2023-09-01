d98748bc71800fe51b25cf5806fd3040b67561c9
Ricardo Morton <rickysosa486@outlook.com>
2023-08-31 22:31:21 -0600

    agregamos ejemplos

---
+++ rickymorton/ejemplo_diccionario.py
@@ -0,0 +1,14 @@
+champions = {
+    "Real Madrid": 14,
+    "Barcelona": 5,
+    "Milan": 7,
+    "Bayer Munich": 7,
+}
+
+# Imprimir un valor específico
+print("Las champions del real madrid son: ", champions["Real Madrid"])
+
+# Agregar un nuevo elemento
+champions["Ajax"] = 5
+
+print(champions)
---
+++ rickymorton/ejemplo_listas.py
@@ -0,0 +1,8 @@
+#ejemplo lista
+ciudades = ['Madrid','Barcelona','Monterrey','CDMX']
+paises = ["España","México"]
+
+for x in paises:
+    ciudades.append(x)
+
+print(ciudades)
