a4e874b1a90ee67f6c709c47841245a61e7cd578
MarcoTorress11 <marco.torress@uanl.edu.mx>
2023-08-31 23:00:43 -0600

    Carpeta marco

---
+++ Marco Torres/ejemplo_conjuntos.py
@@ -0,0 +1,18 @@
+#Crear el conjunto
+lista = set()
+
+#Añadir elementos al conjunto
+lista.add(10)
+lista.add(12)
+lista.add(13)
+lista.add(14)
+lista.add(15)
+print (lista)
+
+#Agregar varios datos a la vez
+lista.update([16,17,18,19,20])
+print (lista)
+
+#Eliminar un elemento del conjunto
+lista.remove(15)
+print (lista)
---
+++ Marco Torres/ejemplo_tupla.py
@@ -0,0 +1,10 @@
+#crear una tupla
+tupla = ("Steelers", "Bills", "Patriots", "Bengals")
+print (tupla)
+
+#imprimir datos especificos de la tulpa
+print (tupla[0])
+print (tupla[3])
+
+#imprimir varios datos de la tupla
+print (tupla[:3])
