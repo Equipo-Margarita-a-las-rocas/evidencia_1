ac02c669710e1f075cef9f1374099a6247bc4867
Frida Valencia <frida.vic.val.reyna@gmail.com>
2023-09-08 23:59:39 -0500

    Ejempli conjuntos

---
+++ Frida_Valencia/ejemplo_conjuntos.py
@@ -0,0 +1,4 @@
+animales_t=set(['Elefante','Jirafa','Perro','Tigre','Leopardo'])
+print('Algunos animales terrestres son: ',animales_t)
+animales_a=set(['Delfin','Ballena azul','Tiburon martillo','Medusa'])
+print('Algunos animales acuaticos son: ',animales_a)
